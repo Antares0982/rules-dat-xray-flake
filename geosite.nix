363c3f9668f45cc24fefff523084c289a95ff278
antares <antares@alyr.dev>
2024-12-01 03:30:39 +0800

    geosite: 0 -> 202411292212
    Diff: https://github.com/Loyalsoldier/v2ray-rules-dat/compare/None...202411292212

--- geosite.nix
+++ geosite.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geosite";
-  version = "0";
+  version = "202411292212";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geosite.dat";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-FE3+g3oUYcxlQk0nSkmrpo6sdJYO9Ej/BpT39jXFyHA=";
   };
 
   unpackPhase = ":";
