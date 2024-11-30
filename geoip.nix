3ea7dcbae8e504a04991b64f105f9e13734acccf
antares <antares@alyr.dev>
2024-12-01 03:30:28 +0800

    geoip: 0 -> 202411292212
    Diff: https://github.com/Loyalsoldier/v2ray-rules-dat/compare/None...202411292212

--- geoip.nix
+++ geoip.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geoip";
-  version = "0";
+  version = "202411292212";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geoip.dat";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-UTSnW3/xfB7JfJGhWiE2OxXeb5HFVLLNhCWlWuq9yaU=";
   };
 
   unpackPhase = ":";
