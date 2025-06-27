fcb91b887151f94920545039a73d0f24a2a4f3ec
aLyrae <alyr@alyr.dev>
2025-06-27 10:27:48 +0000

    geosite: 202506252213 -> 202506262213
    Diff: https://github.com/Loyalsoldier/v2ray-rules-dat/compare/202506252213...202506262213

--- geosite.nix
+++ geosite.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geosite";
-  version = "202506252213";
+  version = "202506262213";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geosite.dat";
-    sha256 = "sha256-51okeZzmet0kzbC7cvjffNAHOE9sV4htqng+toIPMPQ=";
+    sha256 = "sha256-GGW7MtyCUvPJoiAN+HcKs9oRQw9Io8cTn98K9XtTvfg=";
   };
 
   unpackPhase = ":";
