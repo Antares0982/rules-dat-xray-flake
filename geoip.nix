6d5af77045f5a72e36973c01a8b1428cfd888ae7
aLyrae <alyr@alyr.dev>
2025-08-29 10:25:33 +0000

    geoip: 202508272212 -> 202508282212
    Diff: https://github.com/Loyalsoldier/v2ray-rules-dat/compare/202508272212...202508282212

--- geoip.nix
+++ geoip.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geoip";
-  version = "202508272212";
+  version = "202508282212";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geoip.dat";
-    sha256 = "sha256-iWYUhaChIMjaCTsaKHBQlIbHmbcQPDSFVZlin5PAvlE=";
+    sha256 = "sha256-pmOnr3jlh1I9JXz51Or6bwzV+01fv9ti0BT/uG80G6w=";
   };
 
   unpackPhase = ":";
