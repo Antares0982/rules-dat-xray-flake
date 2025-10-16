6e95ded91587de74f38e4605d8911b88ec469651
aLyrae <alyr@alyr.dev>
2025-10-16 10:27:33 +0000

    geoip: 202510142213 -> 202510152212
    Diff: https://github.com/Loyalsoldier/v2ray-rules-dat/compare/202510142213...202510152212

--- geoip.nix
+++ geoip.nix
@@ -5,7 +5,7 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geoip";
-  version = "202510142213";
+  version = "202510152212";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geoip.dat";
