d5ce9d74a9d9b94cb9bda84e7e52df4c6dec01f6
antares <antares@alyr.dev>
2026-01-02 01:47:05 +0800

    Revert "geosite: 202512302215 -> 202512312215"
    This reverts commit d5f45a51eff2cc7e90794a77f6219ebfdc4679a1.

--- geosite.nix
+++ geosite.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xray_geosite";
-  version = "202512312215";
+  version = "202512302215";
 
   src = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${version}/geosite.dat";
-    sha256 = "sha256-VqfWWAqe8kvxRleG6YUZg4cmEGSRTeiXFt6na3c8by8=";
+    sha256 = "sha256-+RyHDRW+deDVfu8+rXWuj7w5MWyOpHNRA7/mCyFQd+g=";
   };
 
   unpackPhase = ":";
